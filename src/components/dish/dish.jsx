a700758e425dd62be99c53d5fe77a5cc6f47cdc8
e.ignatova <e.ignatova@telda.ru>
2025-04-21 11:47:31 +0300

    fix with checking request status in dish component

--- src/components/dish/dish.jsx
+++ src/components/dish/dish.jsx
@@ -6,19 +6,34 @@ import { REQUEST_STATUS_PENDING } from "../redux/constants";
 export const Dish = ({ dishInfo, id, requestStatus }) => {
   const arrayForSkeletons = new Array(3).fill(null);
 
-  const checkStatus = (status) => {
-    if (requestStatus === status) {
-      return arrayForSkeletons.map(() => (
-        <div className={styles.skeletonWrapper}>
-          <Skeleton height={30} />
+  if (requestStatus === REQUEST_STATUS_PENDING) {
+    return (
+      <div>
+        <h2 className={styles.title}>
+          {dishInfo?.name || <Skeleton width={200} />}
+        </h2>
+        <div className={styles.dishInfo}>
+          <div>
+            <h3>Ingredients</h3>
+            <div>
+              {arrayForSkeletons.map(() => (
+                <div className={styles.skeletonWrapper}>
+                  <Skeleton height={30} />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.price}>
+            <h3>Price</h3>
+            <div className={styles.counter}>
+              <DishCounter id={id} />
+              <b>{dishInfo ? dishInfo.price + "$" : <Skeleton width={30} />}</b>
+            </div>
+          </div>
         </div>
-      ));
-    } else {
-      return dishInfo?.ingredients.map((ingredient) => (
-        <div className={styles.ingredient}>{ingredient}</div>
-      ));
-    }
-  };
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -28,7 +43,11 @@ export const Dish = ({ dishInfo, id, requestStatus }) => {
       <div className={styles.dishInfo}>
         <div>
           <h3>Ingredients</h3>
-          <div>{checkStatus(REQUEST_STATUS_PENDING)}</div>
+          <div>
+            {dishInfo?.ingredients.map((ingredient) => (
+              <div className={styles.ingredient}>{ingredient}</div>
+            ))}
+          </div>
         </div>
         <div className={styles.price}>
           <h3>Price</h3>
